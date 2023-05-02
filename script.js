b574866ee2d76e9d1539d821cd5a4f254a173c9d
jerry17john <104645670+jerry17john@users.noreply.github.com>
2023-05-02 03:23:49 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,115 @@
+/*let a= 10;
+let b= 20;
+let c=a+b;
+let d=a-b;
+let e=a*b;
+let f=a%b;
+let g=a/b;
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);*/
+
+/*let a =10;
+let b = "10";
+if(a===b){
+    console.log(true);
+}
+else{
+    console.log(false);
+}*/
+/*
+let f ="changel";
+let s ="jerry";
+let fs = f+s;
+console.log(`$(f) $(fs)`);*/
+/*
+let car ={
+    model1:"5 series",
+    color: "black",
+    price:1000000
+}
+console.log(car);
+console.log(car.color);
+console.log(car.model1);
+console.log(`${car.color} ${car.model1}`);*/
+/*
+let person ={
+    name:"ameya",
+    clg : "christ",
+    dept : "cs"
+}
+console.log(person);*/
+
+// ARRAY
+/*
+let fruit=["apple","mango","orange"];
+fruit[3]="grapes";
+fruit.push("pineapple")
+fruit.unshift()
+fruit.pop();
+console.log(fruit);*/
+/*
+let tudu =[
+    {
+        title:"bestin"
+    },
+    {
+        title:"ameya"
+    },
+    {
+        title:"chanchu"
+    }
+]
+console.log(tudu);*/
+/*
+let std=[
+    {name:"chan",age:18},{name:"ameya",age:19}
+]
+console.log(std[1].name)
+*/
+/*
+function add(a,b){
+    console.log(a+b)
+
+}
+add(2,3)*/
+/*
+function sub(a,b){
+    console.log(a-b)
+
+}
+sub(2,3)*/
+/*
+ add = (a,b) => {
+    sum = a + b
+    if(sum > 10){
+        console.log("large")
+    }
+    else{
+        console.log("small")
+    }
+}
+add(5,11)
+*/
+/*
+let num =[1,2,3,4,5,6,7,8,9,10];
+print = () => {
+    for(i=0;i<num.length;i++){
+        console.log(num[i]);
+    }
+}
+print()
+*/
+/*
+let num =[1,2,3,4,5,6,7,8,9,10];
+print = () => {
+    i=0;
+    while(i<num.length){
+        console.log(num[i]);
+        i++;
+    }
+}
+print()
+*/
